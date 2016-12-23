1f1634960b84a601fa8c55d8d4e074ab26bab544
DLSUmavicreccion <maria_victoria_reccion@dlsu.edu.ph>
2016-12-24 01:35:04 +0800

    fixed login and markasdone

--- ADMDashboard/WebContent/login_files/js/googleSignIn.js
+++ ADMDashboard/WebContent/login_files/js/googleSignIn.js
@@ -46,37 +46,38 @@ var userChanged = function (user) {
   googleUser = user;
 };
 
-$('.login-button').on('click', function() {
-    console.log('signing in');
-    auth2 = gapi.auth2.getAuthInstance({
-      client_id: '20332845396-pvv6j3ifeu0391esi12rdcill7tmq0u7.apps.googleusercontent.com',
-      scope: 'profile'
-  });
-    auth2.signIn().then(function(googleUser) {
-        //callback to handle sign in
-    	 var profile = googleUser.getBasicProfile();console.log('nakapasok po.checking...');
-         console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-         console.log('Full Name: ' + profile.getName());
-         console.log('Given Name: ' + profile.getGivenName());
-         console.log('Family Name: ' + profile.getFamilyName());
-         console.log("Image URL: " + profile.getImageUrl());
-         console.log("Email: " + profile.getEmail());
-         console.log('nakapasok po. working na.');
-         // The ID token you need to pass to your backend:
-         var id_token = googleUser.getAuthResponse().id_token;
-         console.log("ID Token: " + id_token);
- 		
- 		$("#email").val(profile.getEmail());
- 		$("#logoURL").val(profile.getImageUrl());
- 		location.href = "LoginServlet?var=" + profile.getEmail();
- 		//$("#loginform").submit();
- 		
- 		
-    });
-    console.log('nakapasok po.');
-  //  onSignIn(user);
-   
-        
-		
-    });
+//$('.login-button').on('click', function() {
+//    console.log('signing in');
+//    auth2 = gapi.auth2.getAuthInstance({
+//      client_id: '20332845396-pvv6j3ifeu0391esi12rdcill7tmq0u7.apps.googleusercontent.com',
+//      scope: 'profile'
+//  });
+//    auth2.signIn().then(function(googleUser) {
+//        //callback to handle sign in
+//    	 var profile = auth2.getBasicProfile();
+//    	 console.log('nakapasok po.checking...');
+//         console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+//         console.log('Full Name: ' + profile.getName());
+//         console.log('Given Name: ' + profile.getGivenName());
+//         console.log('Family Name: ' + profile.getFamilyName());
+//         console.log("Image URL: " + profile.getImageUrl());
+//         console.log("Email: " + profile.getEmail());
+//         console.log('nakapasok po. working na.');
+//         // The ID token you need to pass to your backend:
+//         var id_token = googleUser.getAuthResponse().id_token;
+//         console.log("ID Token: " + id_token);
+// 		
+// 		$("#email").val(profile.getEmail());
+// 		$("#logoURL").val(profile.getImageUrl());
+// 		location.href = "LoginServlet?var=" + profile.getEmail();
+// 		//$("#loginform").submit();
+// 		
+// 		
+//    });
+//    console.log('nakapasok po.');
+//  //  onSignIn(user);
+//   
+//        
+//		
+//    });
 
